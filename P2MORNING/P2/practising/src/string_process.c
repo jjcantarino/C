83099fbce073118759ef21724f979ec61c63c704
jjcantarino <jjcantarino@gmail.com>
2018-10-17 23:39:22 +0200

    Entr22

--- P2MORNING/P2/practising/src/string_process.c
+++ P2MORNING/P2/practising/src/string_process.c
@@ -46,177 +46,121 @@ int aeroports_process(char* filename,rb_tree * tree){
     }
 }
 
-//rep el path al fitxer per parametre
-//parseja el fitxer
-//emmagatzema a vectors
-//endressa amb qsort (amb el comparador)
-//mostra per pantalla
+//processa l'arxiu dades.csv corresponent a vols
 int dades_process(char* filename, rb_tree * tree){
-   /*We declare variables;*/
-   FILE *fp;            
-   char str[100];       
-   char **vector;
-   char * current_origin;
-   char * current_dest;
-   char * str_delay;
-   int current_delay;
-   int word_length;
-   int line_length;
-   node_data *n_data;
-   list_data * linked_data;
-   int j = 0;
-   int first_idx = 0;
-   int column = 0;
-   int current_column = 0;
-   int nombre_elements = 0, i = 0;
-   // Obrim el fitxer amb filename rebut per parametre
-   fp = fopen(filename , "r");
+     //declaracio de variables
+    FILE *fp;            
+    char str[100];       
+    char **vector;
+    char * current_origin;
+    char * current_dest;
+    char * str_delay;
+    int current_delay;
+    int word_length;
+    int line_length;
+    node_data *n_data;
+    list_data * linked_data;
+    int j = 0;
+    int first_idx = 0;
+    int column = 0;
+    int current_column = 0;
+    int nombre_elements = 0, i = 0;
+    // Obrim el fitxer amb filename rebut per parametre
+    fp = fopen(filename , "r");
 
-   if(fp == NULL) {
-      perror("Error opening file");
-      return(-1);
-   }
-   //vector =  (char **)malloc(nombre_elements*sizeof(char *));
-  
-   //Agafem el nombre d'elements a parsejar, ja que la primera linia es un int amb aquest nombre 
-   //reservem memoria per un vector indicant el tipus de dada, el numero d'elements i l'espai que ocupa una unitat
-  while( fgets (str, 100, fp)!=NULL ) {
-      //convertim a integer
-      line_length = strlen(str);                    
-      first_idx = 0;
-      word_length = 0;
-      column = 0;
-      current_delay = 0;
-      current_origin = NULL;
-      current_dest = NULL;
-      n_data = NULL;
-      linked_data = NULL;
-      for( j = 0; j < line_length; j++){
-          if( str[j] == ',' || str[j] == '\n' ){
-              word_length = j - first_idx;
-              if(word_length > 0){
-                  if(str[j] == '\n'){
-                      str[j] = '\0';
-                      column = 0;
-                  }
-                  if(column==14){//delay         
-                      if ((str_delay = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
-                      strncpy(str_delay, str+first_idx, word_length);          
-                      if(str_delay[0]=='N'){
-                          current_delay = 0;
+    if(fp == NULL) {
+        perror("Error opening file");
+        return(-1);
+    }
+    while( fgets (str, 100, fp)!=NULL ) {
+        //convertim a integer
+        line_length = strlen(str);                    
+        first_idx = 0;
+        word_length = 0;
+        column = 0;
+        current_delay = 0;
+        current_origin = NULL;
+        current_dest = NULL;
+        n_data = NULL;
+        linked_data = NULL;
+        //per cada caracter de la linia
+        for( j = 0 ; j < line_length; j++){
+            //si es , contem la linia, si es \n contemplem el canvi de linia
+            if( str[j] == ',' || str[j] == '\n' ){
+                //el tamany d'aquesta cel la de la taula es igual a 
+                //l'ultim index de ','' trobat menys l'actual index de ','
+                word_length = j - first_idx;
+                if(word_length > 0){
+                    if(str[j] == '\n'){
+                        str[j] = '\0';
+                        column = 0;
+                    }
+                    //si l'index de columna == 14 correspon a una cel la de delay de vol
+                    if( column == 14){//delay         
+                        if ((str_delay = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
+                        // copiem a str_delay el valor corresponent al delay en format string
+                        strncpy(str_delay, str+first_idx, word_length);          
+                        //si aquest string conté una N i no un nombre la cel la es 'NA' i el delay sera 0
+                        if(str_delay[0]=='N'){
+                            current_delay = 0;
 
-                      }else{
-                          current_delay = atoi(str_delay);
-                      }
-                   }
-                  else if(column==16){                
-                      if ((current_origin = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
-                      strncpy(current_origin, str+first_idx, word_length);          
-                  }
-                  else if(column==17){ //destination               
-                      if ((current_dest = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
-                      strncpy(current_dest, str+first_idx, word_length);                  
-                
-                      if ((n_data = malloc(sizeof(node_data)))==0)return report_error();  
-                      if( tree != NULL){
+                        }
+                        else{
+                          //otherwise we convertim el delay a integer
+                            current_delay = atoi(str_delay);
+                        }
+                     }
+                    //si la l'index de la taula es 16 correspon a un codi IATA d'aeroport origen
+                     //guardem a current_origin, per mapejar a la seguent iteracio
+                    else if(column==16){                
+                        if ((current_origin = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
+                        strncpy(current_origin, str+first_idx, word_length);          
+                    }
+                    //finalment, aquesta columna correspon a desti i ja podem afegir un node desti a la llista enllacada
+                    //amb clau origen a l'arbre
+                    else if(column==17){ //destination   
+                        //guardem el codi IATA desti            
+                        if ((current_dest = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
+                        strncpy(current_dest, str+first_idx, word_length);                  
+                  
+                        if ((n_data = malloc(sizeof(node_data)))==0)return report_error();  
+                        if( tree != NULL){
 
-                          n_data = find_node(tree, current_origin);
+                            n_data = find_node(tree, current_origin);
 
-                          if (n_data != NULL) {
-                              //if list_data
-                              linked_data = find_list(n_data->list, current_dest);
-                              if(linked_data != NULL){
-                                  linked_data->minutes = linked_data->minutes + current_delay;
-                                  linked_data->num_flights++;
-                              }
-                              else{
+                            if (n_data != NULL) {
+                                //if list_data
+                                linked_data = find_list(n_data->list, current_dest);
+                                //si existeix llista, vol dir que ja s'ha inicialitzat la llista desti
+                                if(linked_data != NULL){
+                                    //llavors, nomes afegim el delay i incrementem el numero de flights ORIGEN - DESTI
+                                    linked_data->minutes = linked_data->minutes + current_delay;
+                                    linked_data->num_flights++;
+                                }
+                                else{
+                                  //sino, caldra crear la llista amb clau current_dest     
 
-                                  if ((linked_data = malloc(sizeof(list_data)))==0)return report_error();
-                                
-                                  linked_data->key = current_dest;
-                                  linked_data->minutes = current_delay;
-                                  linked_data->num_flights = 1;
-                                  insert_list(n_data->list, linked_data);
-                              }
-                          } 
-                           else {
-                            printf("NOT FOUND: %s \n", current_origin);
-    /*
-                              // If the key is not in the tree, allocate memory for the data
-                              //   and insert in the tree
-
-                              if ((n_data = malloc(sizeof(node_data)))==0)return report_error();
-                              
-                              
-                              // This is the key by which the node is indexed in the tree 
-                              n_data->key = current_origin;
-
-                              if ((n_data->list = malloc(sizeof(list)))==0)return report_error();
-                              
-                              // This is additional information that is stored in the tree 
-
-                              if ((linked_data = malloc(sizeof(list_data)))==0){
-                                  }
-                              if ((linked_data->key = (char *)malloc(word_length*sizeof(char)))==0)return report_error();
-
-                              linked_data->key = current_dest;
-                              linked_data->minutes = current_delay;
-                              linked_data->num_flights++;
-                              insert_list(n_data->list, linked_data);
-                              // We insert the node in the tree 
-                              insert_node(tree, n_data);*/
-                          }      
-                      }
-                  }
-              }
-              first_idx = j + 1;
-              column++;
-          }
-      }
-
-
-
-
-      /*
-      TO DO: 
-            FOR EACH ROW: FIND KEY (ORIGIN) IN TREE, INSERT DESINY IN ITS LINKED LIST
-
-      */
-
-      //assignem l'espai necessari per ficar la cadena a i-essima posicio del vector
-      //i finalment guardem el valor de la cadena modificada al vector
-      
-
-  }
-   /*
-   //Bucle que itera per les seguents linies i coloca al vector la dada rebuda
-   while (fgets (str, 100, fp)!=NULL ) {
-      //per tal d'eliminar el valor \n hem de saber la longitud, i li fiquem un valor \0
-      line_length = strlen(str);                    
-
-      if (line_length > 0 && str[line_length-1] == '\n') {  
-          str[--line_length] = '\0';
-      	  line_length--;
-      }
-
-      //assignem l'espai necessari per ficar la cadena a i-essima posicio del vector
-      //i finalment guardem el valor de la cadena modificada al vector
-      vector[i] = (char *)malloc(line_length*sizeof(char));
-      
-      strcpy(vector[i], str);               
-      i++;
-      
-   }
-   //quicksort c metode
-   qsort(vector, nombre_elements, sizeof(char *), compare_string_length);
-   //per cada element del vector printem per pantalla*/
-   
-   //Important: tancar fitxer i liberar l'espai de cada vector
+                                    if ((linked_data = malloc(sizeof(list_data)))==0)return report_error();
+                                  
+                                    linked_data->key = current_dest;
+                                    linked_data->minutes = current_delay;
+                                    linked_data->num_flights = 1;
+                                    insert_list(n_data->list, linked_data);
+                                }
+                            } 
+                        }
+                    }
+                }
+                first_idx = j + 1;
+                column++;
+            }
+        }
+    }
     fclose(fp);
 
     return(0);
 }
-
+//funcio utilitzada continuament per retornar error de malloc
 int report_error(){
    printf("Not enough space, exiting \n");
    return (-1);
